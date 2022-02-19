d85efbfc0bb351012c9f39bbf3fafc2bae402559
NataliaMoroz0210 <moyanaga@mail.ru>
2022-02-19 22:21:11 +0300

    1

---
+++ src/helpers/MainControllerHelper.java
@@ -0,0 +1,17 @@
+package helpers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainControllerHelper {
+
+//    public void initialize(URL location, ResourceBundle resources) {
+//        // можно по одному добавлять, а можно сразу несколько добавить
+//        comboBox.getItems().setAll(
+//                "Осень",
+//                "Зима",
+//                "Весна",
+//                "Лето"
+//        );
+//    }
+}
